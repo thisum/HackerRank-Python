bb72c1e56f0b80f2c5fed0b6328e25f46eb68c47
thisum <thisum.kb@gmail.com>
2019-08-05 14:26:11 +0530

    optimized version of picking numbers

--- com/thisum/hackerrank/PickingNumbers.py
+++ com/thisum/hackerrank/PickingNumbers.py
@@ -15,6 +15,21 @@ import sys
 
 def pickingNumbers(a):
     # Write your code here
+    a.sort()
+    firstNumber = a[0]
+    count = 0
+    maxCount = 0
+    for i, x in enumerate(a):
+
+        if x - firstNumber <= 1:
+            count += 1
+        else:
+            maxCount = max(maxCount, count)
+            firstNumber = x
+            count = 1
+
+    maxCount = max(maxCount, count)
+    return maxCount
 
 if __name__ == '__main__':
 
