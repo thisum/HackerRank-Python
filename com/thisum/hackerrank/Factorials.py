b1224cef1d158593086da456fbbe1d5d792582a5
thisum <thisum.kb@gmail.com>
2019-08-12 17:08:48 +0530

    factorials using recursion

---
+++ com/thisum/hackerrank/Factorials.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the extraLongFactorials function below.
+def extraLongFactorials(n):
+    if n > 0:
+        return n * extraLongFactorials(n-1)
+    else:
+        return 1
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    fac = extraLongFactorials(n)
+    print(fac)
